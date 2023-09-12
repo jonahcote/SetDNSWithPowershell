459510ab57d82d3fe66654795641c71e539a61fe
Jonah Cote <71891032+jonahcote@users.noreply.github.com>
2023-09-12 15:35:34 -0400

    Updated to specifically select wifi and ethernet

--- script.ps1
+++ script.ps1
@@ -5,9 +5,21 @@ $secondaryDNS = "8.8.8.8"
 # Get the list of network adapters
 $networkAdapters = Get-NetAdapter
 
-# Loop through the network adapters and select the one that is not virtual (associated with VPN)
+# Loop through the network adapters and select the Ethernet adapter
 foreach ($adapter in $networkAdapters) {
-    if (!($adapter.Virtual)) {
+    if ($adapter.Name -like "*Ethernet*") {
+        # Set the DNS server addresses for the selected adapter
+        Set-DnsClientServerAddress -InterfaceAlias $adapter.Name -ServerAddresses ($primaryDNS, $secondaryDNS)
+        
+        Write-Host "DNS settings updated for $($adapter.Name) with primary DNS $primaryDNS and secondary DNS $secondaryDNS."
+        
+        # Exit the loop once the non-VPN adapter is found
+        break
+    }
+}
+# Loop through the network adapters and select the one that is Wi-Fi
+foreach ($adapter in $networkAdapters) {
+    if ($adapter.Name -like "*Wi-Fi*") {
         # Set the DNS server addresses for the selected adapter
         Set-DnsClientServerAddress -InterfaceAlias $adapter.Name -ServerAddresses ($primaryDNS, $secondaryDNS)
         
